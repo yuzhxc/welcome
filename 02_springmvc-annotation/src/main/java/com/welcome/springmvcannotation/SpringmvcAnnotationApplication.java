ec34b7081217422feca3783819a8488e39d0fe1b
yuzhxc <yuzhxc@yonyou.com>
2018-07-27 16:56:49 +0800

    springmvc-annotation准准备中

---
+++ 02_springmvc-annotation/src/main/java/com/welcome/springmvcannotation/SpringmvcAnnotationApplication.java
@@ -0,0 +1,12 @@
+package com.welcome.springmvcannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcAnnotationApplication.class, args);
+	}
+}
